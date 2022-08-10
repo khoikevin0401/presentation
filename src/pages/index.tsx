df547102578b2fedcbfcdfc42181a604bb3c91a7
MinhTuong12101996 <tuong.tran@madison-technology.com>
2022-08-10 11:31:44 +0700

    add commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { ApolloProviderServer } from "@Services/apollo/apollo.services"
 
 import theme from "@Components/common/theme/theme"
 import PresentationUseRef from "@Templates/presentation-useRef"
+import CounterUserReducer from "@Templates/useReducer/CounterUserReducer"
 
 const IndexPage = () => {
   return (
@@ -12,6 +13,7 @@ const IndexPage = () => {
       <ChakraProvider theme={theme}>
         <Router>
           <PresentationUseRef path="/presentation-useref" default/>
+          <CounterUserReducer path="/usereducer" />
         </Router>
       </ChakraProvider>
     </ApolloProviderServer>
